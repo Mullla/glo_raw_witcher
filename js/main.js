8f8261a4263320f6780caa408c00fe55c3305df2
Mars <diablo_13_06@mail.ru>
2021-01-08 19:38:22 +0300

    Update main.js
    added modal window's functional

--- js/main.js
+++ js/main.js
@@ -10,8 +10,31 @@ var mySwiper = new Swiper('.swiper-container', {
     }
   }
 });
+
+
 var menuButton = document.querySelector('.menu-button');
 var menu = document.querySelector('.header');
+
+var overlay = document.querySelector(".overlay");
+var demo = document.querySelector(".demo-modal");
+var demoBtns = document.querySelectorAll(".demo-btn");
+var modalBtn = document.querySelector(".modal-btn");
+
+overlay.style.display='none';
+demo.style.display='none';
+
+
+for(var i = 0; i < demoBtns.length; i++){
+  demoBtns[i].addEventListener('click',function(){
+    overlay.style.display='block';
+    demo.style.display='flex';
+  });
+}
+
+modalBtn.addEventListener('click',function(){
+  overlay.style.display='none';
+  demo.style.display='none';
+});
 
 menuButton.addEventListener('click', function (){
   menuButton.classList.toggle('menu-button-active');
