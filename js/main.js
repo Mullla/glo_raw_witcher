e0903b53ee3020f873e29a5271a5690fba72f15a
Mars <diablo_13_06@mail.ru>
2021-07-25 19:38:26 +0300

    update: js

--- js/main.js
+++ js/main.js
@@ -1,42 +1,42 @@
-var mySwiper = new Swiper('.swiper-container', {
-  slidesPerView: 1,
-  loop: true,
-  navigation: {
-    nextEl: '.arrow',
-  },
-  breakpoints: {
-    540: {
-      slidesPerView: 2,
-    }
-  }
-});
-
-
-var menuButton = document.querySelector('.menu-button');
-var menu = document.querySelector('.header');
-
-var overlay = document.querySelector(".overlay");
-var demo = document.querySelector(".demo-modal");
-var demoBtns = document.querySelectorAll(".demo-btn");
-var modalBtn = document.querySelector(".modal-btn");
-
-overlay.style.display='none';
-demo.style.display='none';
-
-
-for(var i = 0; i < demoBtns.length; i++){
-  demoBtns[i].addEventListener('click',function(){
-    overlay.style.display='block';
-    demo.style.display='flex';
+document.addEventListener("DOMContentLoaded", () => {
+  const mySwiper = new Swiper(".swiper-container", {
+    slidesPerView: 1,
+    loop: true,
+    navigation: {
+      nextEl: ".arrow",
+    },
+    breakpoints: {
+      540: {
+        slidesPerView: 2,
+      },
+    },
   });
-}
 
-modalBtn.addEventListener('click',function(){
-  overlay.style.display='none';
-  demo.style.display='none';
-});
+  const menuButton = document.querySelector(".menu-button");
+  const menu = document.querySelector(".header");
 
-menuButton.addEventListener('click', function (){
-  menuButton.classList.toggle('menu-button-active');
-  menu.classList.toggle('header-active');
+  const overlay = document.querySelector(".overlay");
+  const demo = document.querySelector(".demo-modal");
+  const demoBtns = document.querySelectorAll(".demo-btn");
+  const modalBtn = document.querySelector(".modal-btn");
+
+  overlay.style.display = "none";
+  demo.style.display = "none";
+
+  for (let i = 0; i < demoBtns.length; i++) {
+    demoBtns[i].addEventListener("click", () => {
+      overlay.style.display = "block";
+      demo.style.display = "flex";
+    });
+  }
+
+  modalBtn.addEventListener("click", () => {
+    overlay.style.display = "none";
+    demo.style.display = "none";
+  });
+
+  menuButton.addEventListener("click", () => {
+    menuButton.classList.toggle("menu-button-active");
+    menu.classList.toggle("header-active");
+  });
 });
